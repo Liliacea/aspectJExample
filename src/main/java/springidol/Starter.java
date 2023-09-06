ff425a1bb401586cef2773a5dad1f058c9422a3a
liliacea <Liliacea_85@mail.ru>
2023-09-06 06:21:14 +0300

    right version

--- src/main/java/springidol/Starter.java
+++ src/main/java/springidol/Starter.java
@@ -6,12 +6,14 @@ public class Starter {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
                 "springExample.xml");
-      /*  Performer performer = (Performer) ctx.getBean("smb");
+        Performer performer = (Performer) ctx.getBean("smb");
         performer.perform();
 
-       */
-        Thinker thinker = (Thinker) ctx.getBean("volunteer");
+
+      /*  Thinker thinker = (Thinker) ctx.getBean("volunteer");
         thinker.thinkOfSomething("Aaa");
+
+       */
         ctx.close();
     }
 }
